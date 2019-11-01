3af99175d6e237403c6cbc0cc53495e74d14f586
Maxence Valette <maxence.valette@hotmail.fr>
2019-11-01 09:27:26 +0100

    fix bug

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -8,7 +8,7 @@ class GossipsController < ApplicationController
   end
 
   def create
-  	params[:gossip] = Gossip.new(title: params[:gossip_title],content: params[:gossip_content],user_id: 11)
+  	params[:gossip] = Gossip.new(title: params[:gossip_title],content: params[:gossip_content], user_id: current_user.id)
   	if params[:gossip].save
   		flash.now[:success] = 'Bravo tu as bien enregistré ton Gossip !'
   		redirect_to :action => 'index', notice: 'Thank You For Subscribing!'
